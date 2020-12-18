02fc90ba7a6d43308a5d1f1fd927147b2162aed6
Nguyen Khoa <khoanguyen26777@gmail.com>
2020-12-18 13:40:07 +0700

    commit on '18_12_2020_Khoa'

--- src/app/components/specialized/commecial-managemant/infrastructure/supermarket/supermarket-commecial.component.ts
+++ src/app/components/specialized/commecial-managemant/infrastructure/supermarket/supermarket-commecial.component.ts
@@ -58,7 +58,7 @@ export class SuperMarketCommecialManagementComponent implements OnInit {
 
   public tongSieuThi: number;
   public sieuThiTongHop: number;
-  public sieuThiChuyenNganh: number;
+  public sieuThiChuyenDanh: number;
   //
   public sieuThiHangI: number;
   public sieuThiHangII: number;
@@ -66,6 +66,7 @@ export class SuperMarketCommecialManagementComponent implements OnInit {
   //
   public sieuThiDauTuTrongNam: number;
   public sieuThiDauTuNamTruoc: number;
+  public sieuThiDauTuNamTruoc1: number;
   public sieuThiNgungHoatDong: number;
   public sieuThiDangXayDung: number;
   //
@@ -102,18 +103,221 @@ export class SuperMarketCommecialManagementComponent implements OnInit {
     , { value: "II", text: "Loại II" }
     , { value: "III", text: "Loại III" }]
 
-  headerArray = ['index', 'tenhuyenthi', 'ten_tttm', 'dientich', 'vondautu', 'namdautuxaydung', 'phanloai'];
+  // headerArray = ['index', 'tenhuyenthi', 'ten_tttm', 'dientich', 'vondautu', 'namdautuxaydung', 'phanloai'];
+
+  headerArray = ['index', 'ten_sieu_thi', 'dia_diem', 'dia_ban', 'nha_nuoc', 'ngoai_nha_nuoc', 'co_von_dau_tu_nuoc_ngoai', 'von_khac', 'tong_hop',
+    'chuyen_doanh', 'nam_xay_dung', 'nam_ngung_hoat_dong', 'dien_tich_dat', 'phan_hang', 'so_lao_dong', 'ten_chu_dau_tu',
+    'giay_dang_ky_kinh_doanh', 'dia_chi', 'dien_thoai', 'ho_va_ten', 'dia_chi1', 'dien_thoai1',
+  ];
+
   dataHuyenThi: Array<SuperMarketCommonModel> = [
-    { huyen: "Đồng Xoài", ten_tttm: "Siêu thị Co.opMart Đồng Xoài", dientich: 3107, namdautuxaydung: "2009", phanloai: "II", vondautu: 0, id_quan_huyen: 2 },
-    { huyen: "Đồng Xoài", ten_tttm: "Siêu thị điện máy nội thất Chợ Lớn, chi nhánh Bình Phước", dientich: 4000, namdautuxaydung: "2017", phanloai: "III", vondautu: 0, id_quan_huyen: 2 },
-    { huyen: "Đồng Xoài", ten_tttm: "Siêu thị The Gold Mart ", dientich: 4500, namdautuxaydung: "2019", phanloai: "II", vondautu: 0, id_quan_huyen: 2 },
-    { huyen: "Đồng Xoài", ten_tttm: "Siêu thị điện máy Nguyễn Kim", dientich: 400, namdautuxaydung: "2014", phanloai: "III", vondautu: 0, id_quan_huyen: 2 },
-    { huyen: "Đồng Xoài", ten_tttm: "Siêu thị điện máy xanh Bình Phước", dientich: 3400, namdautuxaydung: "2016", phanloai: "III", vondautu: 0, id_quan_huyen: 2 },
-    { huyen: "Bình Long", ten_tttm: "Siêu thị Bé Lan", dientich: 1000, namdautuxaydung: "2017", phanloai: "III", vondautu: 0, id_quan_huyen: 3 },
-    { huyen: "Phước Long", ten_tttm: "Siêu thị Phương Lan", dientich: 800, namdautuxaydung: "2014", phanloai: "III", vondautu: 0, id_quan_huyen: 1 },
-    { huyen: "Đồng Phú", ten_tttm: "Siêu thị Co.opMart Đồng Phú", dientich: 3000, namdautuxaydung: "2019", phanloai: "II", vondautu: 0, id_quan_huyen: 8 },
-    { huyen: "Bù Đăng", ten_tttm: "Dự án Siêu thị Co.opMart Bù Đăng", dientich: 0, namdautuxaydung: "", phanloai: "", vondautu: 0, id_quan_huyen: 9 },
-    { huyen: "Bù Đăng", ten_tttm: "Dự án Siêu Thị Boobo và chợ đêm", dientich: 0, namdautuxaydung: "", phanloai: "", vondautu: 0, id_quan_huyen: 9 },
+    {
+      ten_sieu_thi: 'Siêu thị Co.opMart Đồng Xoài',
+      dia_diem: 'Đường Phú Riềng Đỏ, phường Tân Bình, thị xã Đồng Xoài, tỉnh Bình Phước',
+      dia_ban: 'Đồng Xoài',
+      nha_nuoc: 30000,
+      ngoai_nha_nuoc: null,
+      co_von_dau_tu_nuoc_ngoai: null,
+      von_khac: null,
+      tong_hop: 'Hàng tiêu dùng',
+      chuyen_doanh: null,
+      nam_xay_dung: '2009',
+      nam_ngung_hoat_dong: null,
+      dien_tich_dat: 3107,
+      phan_hang: 'II',
+      so_lao_dong: 137,
+      ten_chu_dau_tu: 'Công ty TNHH Cao Phong',
+      giay_dang_ky_kinh_doanh: null,
+      dia_chi: null,
+      dien_thoai: null,
+      ho_va_ten: null,
+      dia_chi1: null,
+      dien_thoai1: null
+    },
+    {
+      ten_sieu_thi: 'Siêu thị điện máy nội thất Chợ Lớn, chi nhánh Bình Phước',
+      dia_diem: '658 Phú Riềng Đỏ, KP.Tân Trà, P. Tân Xuân, TX Đồng Xoài, Bình Phước',
+      dia_ban: 'Đồng Xoài',
+      nha_nuoc: null,
+      ngoai_nha_nuoc: 5000,
+      co_von_dau_tu_nuoc_ngoai: null,
+      von_khac: null,
+      tong_hop: null,
+      chuyen_doanh: 'Điện máy, nội thất',
+      nam_xay_dung: '2017',
+      nam_ngung_hoat_dong: null,
+      dien_tich_dat: 4000,
+      phan_hang: 'III',
+      so_lao_dong: 20,
+      ten_chu_dau_tu: 'DNTN Trọng Ngư',
+      giay_dang_ky_kinh_doanh: null,
+      dia_chi: null,
+      dien_thoai: null,
+      ho_va_ten: null,
+      dia_chi1: null,
+      dien_thoai1: null
+    },
+    {
+      ten_sieu_thi: 'Siêu thị Phương Lan',
+      dia_diem: 'Phường Phước Bình, thị xã Phước Long, tỉnh Bình Phước',
+      dia_ban: 'Phước Long',
+      nha_nuoc: null,
+      ngoai_nha_nuoc: 15000,
+      co_von_dau_tu_nuoc_ngoai: null,
+      von_khac: null,
+      tong_hop: 'Hàng tiêu dùng',
+      chuyen_doanh: null,
+      nam_xay_dung: '2014',
+      nam_ngung_hoat_dong: null,
+      dien_tich_dat: 800,
+      phan_hang: 'III',
+      so_lao_dong: 20,
+      ten_chu_dau_tu: 'Công ty TNHH TMDV Sài Gòn - Bình Phước',
+      giay_dang_ky_kinh_doanh: null,
+      dia_chi: null,
+      dien_thoai: null,
+      ho_va_ten: null,
+      dia_chi1: null,
+      dien_thoai1: null
+    },
+    {
+      ten_sieu_thi: 'Siêu thị Co.opMart Đồng Phú',
+      dia_diem: 'thị trấn Tân Phú, huyện Đồng Phú',
+      dia_ban: 'Đồng Phú',
+      nha_nuoc: 20000,
+      ngoai_nha_nuoc: null,
+      co_von_dau_tu_nuoc_ngoai: null,
+      von_khac: null,
+      tong_hop: 'Hàng tiêu dùng',
+      chuyen_doanh: null,
+      nam_xay_dung: '2019',
+      nam_ngung_hoat_dong: null,
+      dien_tich_dat: 3000,
+      phan_hang: 'II',
+      so_lao_dong: 50,
+      ten_chu_dau_tu: 'Công ty Cổ phần TM DV The Gold Mart',
+      giay_dang_ky_kinh_doanh: null,
+      dia_chi: null,
+      dien_thoai: null,
+      ho_va_ten: null,
+      dia_chi1: null,
+      dien_thoai1: null
+    },
+    {
+      ten_sieu_thi: 'Siêu thị The Gold Mart ',
+      dia_diem: 'đường Tôn Đức Thắng, ấp 2, xã Tiến Thành, thị xã Đồng Xoài, tỉnh Bình Phước',
+      dia_ban: 'Đồng Xoài',
+      nha_nuoc: null,
+      ngoai_nha_nuoc: 40000,
+      co_von_dau_tu_nuoc_ngoai: null,
+      von_khac: null,
+      tong_hop: 'Hàng tiêu dùng',
+      chuyen_doanh: null,
+      nam_xay_dung: '2019',
+      nam_ngung_hoat_dong: null,
+      dien_tich_dat: 4500,
+      phan_hang: 'II',
+      so_lao_dong: null,
+      ten_chu_dau_tu: 'CÔNG TY CỔ PHẦN THẾ GIỚI DI ĐỘNG',
+      giay_dang_ky_kinh_doanh: null,
+      dia_chi: null,
+      dien_thoai: null,
+      ho_va_ten: null,
+      dia_chi1: null,
+      dien_thoai1: null
+    },
+    {
+      ten_sieu_thi: 'Siêu thị điện máy xanh Bình Phước',
+      dia_diem: 'Phường Tân Thiện, thị xã Đồng Xoài,',
+      dia_ban: 'Đồng Xoài',
+      nha_nuoc: null,
+      ngoai_nha_nuoc: 50000,
+      co_von_dau_tu_nuoc_ngoai: null,
+      von_khac: null,
+      tong_hop: null,
+      chuyen_doanh: 'Điện máy, nội thất',
+      nam_xay_dung: '2016',
+      nam_ngung_hoat_dong: null,
+      dien_tich_dat: 3400,
+      phan_hang: 'III',
+      so_lao_dong: null,
+      ten_chu_dau_tu: 'Công ty TNHH MTV siêu thị Gia Đình',
+      giay_dang_ky_kinh_doanh: null,
+      dia_chi: null,
+      dien_thoai: null,
+      ho_va_ten: null,
+      dia_chi1: null,
+      dien_thoai1: null
+    },
+    {
+      ten_sieu_thi: 'Siêu thị Bé Lan',
+      dia_diem: 'Phường An Lộc, thị xã Bình Long',
+      dia_ban: 'Bình Long',
+      nha_nuoc: null,
+      ngoai_nha_nuoc: 15000,
+      co_von_dau_tu_nuoc_ngoai: null,
+      von_khac: null,
+      tong_hop: 'Hàng tiêu dùng',
+      chuyen_doanh: null,
+      nam_xay_dung: '2017',
+      nam_ngung_hoat_dong: null,
+      dien_tich_dat: 1000,
+      phan_hang: 'III',
+      so_lao_dong: 35,
+      ten_chu_dau_tu: 'Công ty TNHH TMDV Sài Gòn - Bình Phước',
+      giay_dang_ky_kinh_doanh: null,
+      dia_chi: null,
+      dien_thoai: null,
+      ho_va_ten: null,
+      dia_chi1: null,
+      dien_thoai1: null
+    },
+    {
+      ten_sieu_thi: 'Dự  án Siêu thị Co.opMart Bù Đăng',
+      dia_diem: 'thị trấn Đức Phong, huyện Bù Đăng, tỉnh Bình Phước',
+      dia_ban: 'Bù Đăng',
+      nha_nuoc: null,
+      ngoai_nha_nuoc: null,
+      co_von_dau_tu_nuoc_ngoai: null,
+      von_khac: null,
+      tong_hop: null,
+      chuyen_doanh: null,
+      nam_xay_dung: null,
+      nam_ngung_hoat_dong: 'đang được UBND thuận chủ trương thực hiện',
+      dien_tich_dat: 6000,
+      phan_hang: null,
+      so_lao_dong: null,
+      ten_chu_dau_tu: 'đang được UBND thuận chủ trương thực hiện',
+      giay_dang_ky_kinh_doanh: null,
+      dia_chi: null,
+      dien_thoai: null,
+      ho_va_ten: null,
+      dia_chi1: null,
+      dien_thoai1: null
+    },
+    {
+      ten_sieu_thi: 'Dự án Siêu Thị Bombo và chợ đêm',
+      dia_diem: 'thôn 4, xã Bomboo, huyện Bù Đăng, tỉnh Bình Phước',
+      dia_ban: 'Bù Đăng',
+      nha_nuoc: null,
+      ngoai_nha_nuoc: null,
+      co_von_dau_tu_nuoc_ngoai: null,
+      von_khac: null,
+      tong_hop: null,
+      chuyen_doanh: null,
+      nam_xay_dung: null,
+      nam_ngung_hoat_dong: 'đang được UBND thuận chủ trương thực hiện',
+      dien_tich_dat: 4800,
+      phan_hang: null,
+      so_lao_dong: null,
+      ten_chu_dau_tu: null,
+      giay_dang_ky_kinh_doanh: null,
+      dia_chi: null,
+      dien_thoai: null,
+      ho_va_ten: null,
+      dia_chi1: null,
+      dien_thoai1: null
+    }
   ]
   //Variable for only TS-------------------------------------------------------------------------
   supermarketTypeI: number = 0;
@@ -125,18 +329,18 @@ export class SuperMarketCommecialManagementComponent implements OnInit {
 
   filterTyppeMarket() {
     this.dataHuyenThi.forEach(element => {
-      switch (element.phanloai) {
+      switch (element.phan_hang) {
         case "I":
-          this.supermarketTypeI +=1;
+          this.supermarketTypeI += 1;
           break;
         case "II":
-          this.supermarketTypeII+=1;
+          this.supermarketTypeII += 1;
           break;
         case "III":
-          this.supermarketTypeIII+=1;
+          this.supermarketTypeIII += 1;
           break;
         case "":
-          this.supermarketFuture +=1;
+          this.supermarketFuture += 1;
           break;
         default:
           break;
@@ -256,14 +460,19 @@ export class SuperMarketCommecialManagementComponent implements OnInit {
   }
   private _caculator(data: Array<SuperMarketCommonModel>) {
     this.tongSieuThi = data.length;
-    this.sieuThiHangI = data.filter(x => x.phanloai == "I").length;
-    this.sieuThiHangII = data.filter(x => x.phanloai == "II").length;
-    this.sieuThiHangIII = data.filter(x => x.phanloai == "III").length;
+    this.sieuThiHangI = data.filter(x => x.phan_hang == "I").length;
+    this.sieuThiHangII = data.filter(x => x.phan_hang == "II").length;
+    this.sieuThiHangIII = data.filter(x => x.phan_hang == "III").length;
     this.sieuThiDangXayDung = data.length - this.sieuThiHangI - this.sieuThiHangII - this.sieuThiHangIII;
     this.year = new Date().getFullYear();
 
-    this.sieuThiDauTuTrongNam = data.filter(x => x.namdautuxaydung == this.year.toString()).length;
-    this.sieuThiDauTuNamTruoc = data.filter(x => x.namdautuxaydung == (this.year - 1).toString()).length;
+    this.sieuThiDauTuTrongNam = data.filter(x => x.nam_xay_dung == this.year.toString()).length;
+    this.sieuThiDauTuNamTruoc = data.filter(x => x.nam_xay_dung == (this.year - 1).toString()).length;
+    this.sieuThiDauTuNamTruoc1 = Math.abs(this.sieuThiDauTuTrongNam - this.sieuThiDauTuNamTruoc);
+
+    this.sieuThiChuyenDanh = data.filter(x => x.chuyen_doanh != null).length;
+    this.sieuThiTongHop = data.filter(x => x.tong_hop != null).length;
+
     this.filteredDataSource.data = [...data];
   }
   private _paginatorAgain() {
